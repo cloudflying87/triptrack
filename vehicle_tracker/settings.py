088574eb0c36692b1bfc0e672d37d8f3b021f8a8
David Hale <davidhale87@gmail.com>
2025-04-22 15:29:31 -0700

    Docker change

--- vehicle_tracker/settings.py
+++ vehicle_tracker/settings.py
@@ -66,7 +66,7 @@ WSGI_APPLICATION = 'vehicle_tracker.wsgi.application'
 # Database
 DATABASES = {
     'default': dj_database_url.config(
-        default=f"postgres://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT', '5432')}/{os.getenv('DB_NAME')}",
+        default=f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT', '5432')}/{os.getenv('DB_NAME')}",
         conn_max_age=600
     )
 }
