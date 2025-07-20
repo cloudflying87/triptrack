ebdfb66dad7f1f8dcdebbe8ef212a46e6df6f1d8
David Hale <davidhale87@gmail.com>
2025-07-19 22:11:36 -0500

    Fixing database settings

--- vehicle_tracker/settings.py
+++ vehicle_tracker/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'vehicle_tracker.wsgi.application'
 # Database
 DATABASES = {
     'default': dj_database_url.config(
-        default=f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT', '5432')}/{os.getenv('DB_NAME')}",
+        default='sqlite:///db.sqlite3',
         conn_max_age=600
     )
 }
